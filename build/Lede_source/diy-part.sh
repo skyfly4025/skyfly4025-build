572f437e0737a8ed06b43194fee401c75f4120f0
skyfly4025 <72966064+skyfly4025@users.noreply.github.com>
2021-12-14 17:31:09 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -7,9 +7,9 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-svn co https://github.com/kiddin9/openwrt-bypass/tree/main/luci-app-bypass package/luci-app-bypass
-svn co https://github.com/kiddin9/openwrt-bypass/tree/main/lua-maxminddb package/lua-maxminddb
-svn co https://github.com/kenzok8/small/tree/master/kcptun package/kcptun
+svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
+svn co https://github.com/kiddin9/openwrt-bypass/trunk/lua-maxminddb package/lua-maxminddb
+svn co https://github.com/kenzok8/small/trunk/kcptun package/kcptun
 
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='10.10.10.1'                                    # IPv4 地址(openwrt后台地址)
