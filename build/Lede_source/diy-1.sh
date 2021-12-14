a2574d44372ee278bcd3e8468718099ac11b79e7
skyfly4025 <72966064+skyfly4025@users.noreply.github.com>
2021-12-14 16:53:36 +0800

    Delete diy-1.sh

--- build/Lede_source/diy-1.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/kiddin9/openwrt-bypass/tree/main/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/kiddin9/openwrt-bypass/tree/main/lua-maxminddb package/lua-maxminddb
-git clone https://github.com/kenzok8/small/tree/master/kcptun package/kcptun
