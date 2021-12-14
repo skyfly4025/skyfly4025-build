10d68f7a0a13a49d12ade79424fab3a528480f37
skyfly4025 <72966064+skyfly4025@users.noreply.github.com>
2021-12-14 16:48:09 +0800

    Update and rename diy-part.sh to diy-2.sh

--- build/Lede_source/diy-2.sh
+++ build/Lede_source/diy-2.sh
@@ -6,10 +6,6 @@
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
-git clone https://github.com/kiddin9/openwrt-bypass/tree/main/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/kiddin9/openwrt-bypass/tree/main/lua-maxminddb package/lua-maxminddb
-git clone https://github.com/kenzok8/small/tree/master/kcptun package/kcptun
-
 
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='10.10.10.1'                                    # IPv4 地址(openwrt后台地址)
